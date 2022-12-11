3ecb79e1f07689e1a22bb841a5830d501bc23d69
Esteban Mortier <64356124+Esteban-Mo@users.noreply.github.com>
2022-12-12 00:22:36 +0100

    Update sending page

--- src/app/components/sending-page/sending-page.component.ts
+++ src/app/components/sending-page/sending-page.component.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 import { PostsService } from '@src/services/apis/posts.services';
 
+interface SocialMedia {
+	key: string;
+	text: string;
+	isActive: boolean;
+	iconId: string;
+}
+
 @Component({
 	selector: 'app-sending-page',
 	templateUrl: './sending-page.component.html',
@@ -8,115 +15,56 @@ import { PostsService } from '@src/services/apis/posts.services';
 })
 export class SendingPageComponent {
 	ngChar: number = 0;
-	text!: string;
 	date!: Date;
 
-	socialMedia = {
-		facebook: {
+	readonly FACEBOOK_KEY = 'facebook';
+	readonly TWITTER_KEY = 'twitter';
+	readonly INSTAGRAM_KEY = 'instagram';
+	readonly LINKEDIN_KEY = 'linkedin';
+
+	socialMedia: SocialMedia[] = [
+		{
+			key: this.FACEBOOK_KEY,
 			text: 'Facebook',
 			isActive: true,
 			iconId: 'facebook-icon',
 		},
-		twitter: {
+		{
+			key: this.TWITTER_KEY,
 			text: 'Twitter',
 			isActive: false,
 			iconId: 'twitter-icon',
 		},
-		instagram: {
+		{
+			key: this.INSTAGRAM_KEY,
 			text: 'Instagram',
 			isActive: false,
 			iconId: 'instagram-icon',
 		},
-		linkedin: {
+		{
+			key: this.LINKEDIN_KEY,
 			text: 'LinkedIn',
 			isActive: false,
 			iconId: 'linkedin-icon',
 		},
-	};
+	];
 
-	constructor(private postsService: PostsService) {}
+	constructor() {}
 
-	ngOnInit(): void {
-		console.log(this.socialMedia);
-		this.disableIcon(Object.values(this.socialMedia));
-	}
+	ngOnInit(): void {}
 
-	// NOT USED
-	textChange(event: any) {
-		this.text = event.target.value;
-		this.ngChar = this.text.length;
-
-		this.facebookCheck();
-		this.twitterCheck();
-	}
-	// NOT USED
+	//////////////////////////////////////////////////
+	// Functions
+	//////////////////////////////////////////////////
 
 	dateChange(event: any) {
-		this.date = new Date(event.target.value);
-		this.date.setHours(this.date.getHours() + 1);
-	}
+		// Convert the input value to a Date object
+		const date = new Date(event.target.value);
 
-	nextMedia() {
-		console.log(this.socialMedia);
-		this.setNextActive(Object.values(this.socialMedia));
-		console.log(this.socialMedia);
-	}
+		// Add one hour to the date
+		date.setHours(date.getHours() + 1);
 
-	setNextActive(items: { text: string; isActive: boolean; iconId: string }[]): void {
-		// Find the currently active item
-		const activeIndex = items.findIndex((item) => item.isActive);
-		if (activeIndex === -1) return; // No active item, stop the function
-
-		// Set the current active item to inactive
-		items[activeIndex].isActive = false;
-
-		// Set the next item in the list to active (or the first item if it was the last one)
-		items[(activeIndex + 1) % items.length].isActive = true;
-
-		this.disableIcon(items);
-	}
-
-	disableIcon(items: { text: string; isActive: boolean; iconId: string }[]) {
-		items.forEach((item) => {
-			if (!item.isActive) {
-				document.getElementById(item.iconId)!.classList.add('disable-social');
-			} else {
-				document.getElementById(item.iconId)!.classList.remove('disable-social');
-			}
-		});
-	}
-
-	sendPost() {
-		if (!this.text) {
-			alert('Entrez un texte pour envoyer le post');
-			return;
-		}
-
-		if (!confirm('Êtes-vous sûr de vouloir envoyer ce message ?')) return;
-
-		this.postsService.createPost({ message: this.text, facebook: this.facebookCheck(), twitter: false, date: this.date }).subscribe((res) => {
-			console.log(res);
-		});
-	}
-
-	facebookCheck() {
-		if (this.text.length > 20000) {
-			console.log('this.text');
-			document.getElementById('facebook-icon')!.classList.add('disable-social');
-			return false;
-		}
-
-		document.getElementById('facebook-icon')!.classList.remove('disable-social');
-		return true;
-	}
-
-	twitterCheck() {
-		if (this.text.length > 280) {
-			document.getElementById('twitter-icon')!.classList.add('disable-social');
-			return false;
-		}
-
-		document.getElementById('twitter-icon')!.classList.remove('disable-social');
-		return true;
+		// Set the date property to the updated date
+		this.date = date;
 	}
 }
